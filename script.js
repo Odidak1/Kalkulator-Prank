0e6ae08708ce739b5bd9a919f5dc18ad3a2aeb33
Odidak1 <raditm100308@gmail.com>
2024-06-02 11:52:50 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+  document.getElementById('screen').value = "I love you♥️♥️♥️♥️";
+}
+function appendToScreen(value) {
+  document.getElementById('screen').value += value;
+}
+function clearScreen() {
+  document.getElementById('screen').value = '';
+}
